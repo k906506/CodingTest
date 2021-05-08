80a6b5226d0bd6f76fa6d7a0372242c1adf38bff
Dohyeon Ko <33795856+k906506@users.noreply.github.com>
2021-05-08 17:56:24 +0900

    Complete

--- 2021 카카오 인턴십/1.py
+++ 2021 카카오 인턴십/1.py
@@ -11,15 +11,15 @@ def solution(s):
             temp += s[i]
         if temp in e_numbers:
             temp = temp.replace('one', '1')
-            temp =temp.replace('two', '2')
-            temp =temp.replace('three', '3')
-            temp =temp.replace('four', '4')
-            temp =temp.replace('five', '5')
-            temp =temp.replace('six', '6')
-            temp =temp.replace('seven', '7')
-            temp =temp.replace('eight', '8')
-            temp =temp.replace('nine', '9')
-            temp =temp.replace('zero', '0')
+            temp = temp.replace('two', '2')
+            temp = temp.replace('three', '3')
+            temp = temp.replace('four', '4')
+            temp = temp.replace('five', '5')
+            temp = temp.replace('six', '6')
+            temp = temp.replace('seven', '7')
+            temp = temp.replace('eight', '8')
+            temp = temp.replace('nine', '9')
+            temp = temp.replace('zero', '0')
             answer += temp
             temp = ''
 
